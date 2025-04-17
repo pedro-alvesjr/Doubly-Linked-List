d1df7af307faf590cd97c572afc6ce9e0c1ba0ae
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-16 23:11:05 -0300

    Update DoublyLinkedList.py
    Set_value() and get() methods were added.

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -66,32 +66,60 @@ class DoublyLinkedList:
         else:
             self.head = self.head.next
             self.head.prev = None
-            temp.next = None
-        self.length -= 1 
+            temp.next = None      
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev  
         return temp
         
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        else:
+            return False
 
 
+my_doubly_linked_list = DoublyLinkedList(11)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(23)
+my_doubly_linked_list.append(7)
 
+print('DLL before set_value():')
+my_doubly_linked_list.print_list()
 
-my_doubly_linked_list = DoublyLinkedList(2)
-my_doubly_linked_list.append(1)
+my_doubly_linked_list.set_value(1,4)
 
-
-# (2) Items - Returns 2 Node
-print(my_doubly_linked_list.pop_first().value)
-# (1) Item -  Returns 1 Node
-print(my_doubly_linked_list.pop_first().value)
-# (0) Items - Returns None
-print(my_doubly_linked_list.pop_first())
+print('\nDLL after set_value():')
+my_doubly_linked_list.print_list()
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    2
-    1
-    None
+    DLL before set_value():
+    11
+    3
+    23
+    7
+
+    DLL after set_value():
+    11
+    4
+    23
+    7
 
 """
